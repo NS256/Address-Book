33bf9ee58c5a0f8361f729c0ac5c1d9e3e8915e2
Nathan Smith <nls999@icloud.com>
2025-08-17 14:29:33 +0100

    Added getter and setter methods to Contact.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,31 +1,39 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.TreeMap;
 
 public class AddressBook {
     // This class can be used to manage a collection of contacts
     // For now, it is empty and can be expanded later
-    public static TreeMap<String, Contact> addressBook = new TreeMap<>();
-    Scanner input = new Scanner(System.in);
+    private static TreeMap<String, Contact> addressBook = new TreeMap<>();
+    //Scanner input = new Scanner(System.in);
 
     public void clearAddressBook() {
         addressBook.clear();    
         System.out.println("Address Book cleared.");
     }
 
-    public static boolean createContact(String name, Contact contact){
+    public static void createContact(Contact contact){
+        String name = Contact.getFullName(contact);
+
         if(addressBook.containsKey(name)){
             System.out.print(name + " already exists in your address book, you'll need to create a different name instead.");
-            return false;
+            return;
         }
 
         try {
             addressBook.put(name, contact);
             System.out.println(name + " has been added to your Address Book.");
-            return true;
             
         } catch (Exception e) {
-            System.out.println("Failed to add contact to address book.\f" +e);
-            return false;
+            System.out.println("Failed to add contact to address book.\f" + e);
         }
+    }
+
+    public static void updateAddressBook(String key, Contact contact){
+        addressBook.put(key, contact);
+    }
+
+    public static Contact findContact(String searchString){
+        return addressBook.get(searchString);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ public class Main {
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
         System.out.println("Welcome to your Address Book!");
-        Contact contact = new Contact("John", "Doe", "123-456-7890", "email@address.com");
-        Contact contact2 = new Contact("Jane", "Smith", "098-765-4321", "email@address.com");
+
+        
 
 
         Contact.create("Nathan","Smith","07415266178","nls999@icloud.com");
 
+        System.out.println(Contact.getFirstName(AddressBook.findContact("Nathan Smith")));
 
-        System.out.println(AddressBook.addressBook);
+        Contact.updateFirstName("Nath", AddressBook.findContact("Nathan Smith"));
+
+        System.out.println(Contact.getFirstName(AddressBook.findContact("Nath Smith")));
     }
 }
