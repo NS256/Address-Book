4364c7f5b957af8ff4c75910b92beb7c17c42d00
Nathan Smith <nls999@icloud.com>
2025-08-29 18:03:41 +0100

    Updated Save.java to create a CSV file instead of a txt file - this may make it easier to read details but if not will make it easier to open in another program should that be needed.

--- src/Save.java
+++ src/Save.java
@@ -1,15 +1,19 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.TreeMap;
 
 public class Save {
     public static boolean fileExists = false;
+    private static File saveFile;
 
     public static void createSaveFile(){
         LocalDateTime currentDateTime = LocalDateTime.now();
+        
 
         try {
-            File saveFile = new File("AddressBook.txt");
+            saveFile = new File("AddressBook.csv");
             saveFile.createNewFile();
 
             if(saveFile.createNewFile()){
@@ -24,4 +28,81 @@ public class Save {
             System.out.print("Failed to create Address Book.\n" + e);
         }
     }
+
+    public static void updateSaveFile(TreeMap<String, Contact> addressBook){
+
+        //convert the contacts inside address books to string so they can be saved
+        TreeMap<String,String> stringedAddressBook = new TreeMap<String,String>();
+
+        //createString to be added to CSV file
+        String csvAddressBook = new String();
+        //add header files to CSV string
+        csvAddressBook += "firstName,lastName,emailAddress,phoneNo\n";
+
+
+        for (String key : addressBook.keySet()){
+            //stringedAddressBook.put(key,createStringForSave(addressBook.get(key)));
+
+            csvAddressBook += createStringForSave(addressBook.get(key));
+        }
+
+
+        try {
+            FileWriter updateFile = new FileWriter(saveFile.getPath());
+            updateFile.write(csvAddressBook/*String.valueOf(stringedAddressBook)*/);
+            updateFile.close();
+
+        } catch (IOException e) {
+            String errorFilePath = Log.createLogFile();
+            System.out.println("Failed to update address file, changes haven't been saved...\n" + e);
+            
+            e.printStackTrace();
+
+            Log.addLoggingDetails(errorFilePath, e, "Failed to update address file, changes haven't been saved...");
+
+        }
+        
+
+    }
+
+    public static String createStringForSave(Contact contact){
+        String contactString = new String();
+
+        contactString += contact.getFirstName() + ",";
+        contactString += contact.getLastName() + ",";
+        contactString += contact.getEmailAddress() + ",";
+        contactString += contact.getPhoneNo() + "\n";
+
+        
+        /*String[] contactArr = new String[4];
+
+        contactArr[0] = contact.getFirstName();
+        contactArr[1] = contact.getLastName();
+        contactArr[2] = contact.getPhoneNo();
+        contactArr[3] = contact.getEmailAddress();*/
+
+        return contactString; //Arrays.toString(contactArr);
+    }
+
+    /*public static TreeMap<String,Contact> loadSaveFile(){
+        TreeMap<String,Contact> output = new TreeMap<String,Contact>();
+
+        try {
+            String addressBookString = "";
+            Scanner readAddressbook = new Scanner(saveFile);
+            TreeMap<String,String> stringedAddressBook = new TreeMap<String,String>();
+
+            while(readAddressbook.hasNextLine()){
+                addressBookString += readAddressbook.nextLine();
+            }
+            readAddressbook
+
+
+        } catch (IOException e) {
+        }
+
+        
+
+        return output;
+    }*/
 }
